05569532820910eb6b37fba1bf69cd6c07f8af3a
matiroza <44197735+matiroza@users.noreply.github.com>
2021-08-05 10:36:59 +0200

    dokonczony splash screen

--- app/src/main/java/com/example/projekt/MainActivity.java
+++ app/src/main/java/com/example/projekt/MainActivity.java
@@ -5,8 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //hooks
+    View first, second, third, fourth, fifth;
+    TextView created, appName;
+
+    //Animacje
+    Animation topAnimation, bottomAnimation, middleAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +28,35 @@ public class MainActivity extends AppCompatActivity {
 
         if(getSupportActionBar() != null) getSupportActionBar().hide(); //wylaczenie action bara
 
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); //usuniecie gornego menu
+
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top);
+        middleAnimation = AnimationUtils.loadAnimation(this, R.anim.middle);
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom);
+
+        //hooks
+        first = findViewById(R.id.first);
+        second = findViewById(R.id.second);
+        third = findViewById(R.id.third);
+        fourth = findViewById(R.id.fourth);
+        fifth = findViewById(R.id.fifth);
+
+        created = findViewById(R.id.created);
+        appName = findViewById(R.id.appName);
+
+
+        //animacje
+        first.setAnimation(topAnimation);
+        second.setAnimation(topAnimation);
+        third.setAnimation(topAnimation);
+        fourth.setAnimation(topAnimation);
+        fifth.setAnimation(topAnimation);
+
+        created.setAnimation(bottomAnimation);
+
+        appName.setAnimation(middleAnimation);
+
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -22,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, 3000);
+        }, 4000);
 
     }
 }
