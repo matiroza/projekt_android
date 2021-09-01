554e5e0dde2e1e15849fbfd125814d48d8633d42
matiroza <44197735+matiroza@users.noreply.github.com>
2021-09-01 12:08:46 +0200

    gps

--- app/src/main/java/com/example/projekt/MapFragment.java
+++ app/src/main/java/com/example/projekt/MapFragment.java
@@ -1,40 +1,111 @@
 package com.example.projekt;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.VectorDrawable;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MapFragment extends Fragment {
-    /**
-     * Called to have the fragment instantiate its user interface view.
-     * This is optional, and non-graphical fragments can return null. This will be called between
-     * {@link #onCreate(Bundle)} and {@link #onViewCreated(View, Bundle)}.
-     * <p>A default View can be returned by calling  in your
-     * constructor. Otherwise, this method returns null.
-     *
-     * <p>It is recommended to <strong>only</strong> inflate the layout in this method and move
-     * logic that operates on the returned View to {@link #onViewCreated(View, Bundle)}.
-     *
-     * <p>If you return a View from here, you will later be called in
-     * {@link #onDestroyView} when the view is being released.
-     *
-     * @param inflater           The LayoutInflater object that can be used to inflate
-     *                           any views in the fragment,
-     * @param container          If non-null, this is the parent view that the fragment's
-     *                           UI should be attached to.  The fragment should not add the view itself,
-     *                           but this can be used to generate the LayoutParams of the view.
-     * @param savedInstanceState If non-null, this fragment is being re-constructed
-     *                           from a previous saved state as given here.
-     * @return Return the View for the fragment's UI, or null.
-     */
+
+
+    Button btnLocation;
+    TextView textView1, textView2, textView3, textView4, textView5;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        btnLocation = view.findViewById(R.id.btn_location);
+        textView1 = view.findViewById(R.id.text_location_1);
+        textView2 = view.findViewById(R.id.text_location_2);
+        textView3 = view.findViewById(R.id.text_location_3);
+        textView4 = view.findViewById(R.id.text_location_4);
+        textView5 = view.findViewById(R.id.text_location_5);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        btnLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    getLocation();
+                } else {
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+                }
+            }
+        });
+
+        return view;
     }
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                Location location = task.getResult();
+                if (location != null) {
+                    Geocoder geocoder = new Geocoder(getContext(), Locale.getDefault());
+                    try {
+                        List<Address> addressList = geocoder.getFromLocation(
+                                location.getLatitude(), location.getLongitude(), 1
+                        );
+                        textView1.setText(Html.fromHtml(
+                                "<font color='#6200EE'><b>Latitude :</b><br></font>" +
+                                        addressList.get(0).getLatitude()
+                        ));
+                        textView2.setText(Html.fromHtml(
+                                "<font color='#6200EE'><b>Longitude :</b><br></font>" +
+                                        addressList.get(0).getLatitude()
+                        ));
+                        textView3.setText(Html.fromHtml(
+                                "<font color='#6200EE'><b>Country :</b><br></font>" +
+                                        addressList.get(0).getCountryName()
+                        ));
+                        textView4.setText(Html.fromHtml(
+                                "<font color='#6200EE'><b>Locality :</b><br></font>" +
+                                        addressList.get(0).getLocality()
+                        ));
+                        textView5.setText(Html.fromHtml(
+                                "<font color='#6200EE'><b>Adress :</b><br></font>" +
+                                        addressList.get(0).getAddressLine(0)
+                        ));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+
+
+
 }
